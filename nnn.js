62ca016a427dfd80caa6e69c1d965f29b1d1f8f0
Markus Bayer <bayer_markus@web.de>
2016-12-15 21:18:54 +0100

    working with waitForSelector()

--- nnn.js
+++ nnn.js
@@ -2,38 +2,78 @@ var urlProduct = 'https://www.amazon.de/Aktiv-Organic-Ashwagandha-Pulver-250g/dp
 
 var casper = require('casper').create();
 // required in ordner to retrieve amazon cookies
-// casper.userAgent('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)');
-casper.userAgent('Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36');
+// macOS Chrome
+casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36');
+
+function getHTML(fileName) {
+    casper.then(function() {
+        var fs = require('fs');
+        var html = this.getHTML();
+        var f = fs.open(fileName, 'w');
+        f.write(html);
+        f.close();
+    });
+}
 
 // open product page
 casper.start(urlProduct);
+console.log('shopping! 💁 👗 💍 👠 👛')
 console.log('visiting ', urlProduct);
 
 casper.then(function() {
-    this.capture('product.png');
-    console.log('adding product to cart');
+    this.capture('01.png');
+    console.log('adding product to cart 🎁');
     this.click('#add-to-cart-button');
-    this.capture('cart-add.png');
+    getHTML('01.html');
 });
 
 casper.then(function() {
-    console.log('visiting cart');
-    this.click('#hlb-view-cart-announce');
-    this.capture('cart.png');
+console.log('visiting cart 🛍');
+    this.click('#hlb-view-cart');
+    this.capture('02.png');
+    getHTML('02.html');
 });
 
 casper.then(function() {
-    console.log('setting amount of items to 999');
+    console.log('setting amount of items to 999 😎 💳');
+    console.log('click dropdown');
     this.click('#a-autoid-0-announce');
+    this.capture('03.png');
+    getHTML('03.html');
+});
+
+casper.waitForSelector('#dropdown1_9', function() {
+// casper.then(function() {
+    console.log('click 10+');
     this.click('#dropdown1_9');
-    this.sendKeys('activeCartViewForm > div.sc-list-body > div > div.sc-list-item-content > div.a-row.a-spacing-base.a-spacing-top-base > div.a-column.a-span2.a-text-right.sc-action-links.a-span-last > div > div > input', '999');
-    this.click('a-autoid-1-announce');
-    this.capture('amount.png');
+    this.capture('04.png');
+    getHTML('04.html');
 });
 
 casper.then(function() {
-    console.log('selfie!');
-    this.capture('result.png');
+    console.log('sending keys');
+    this.sendKeys('#activeCartViewForm > div.sc-list-body > div > div.sc-list-item-content > div.a-row.a-spacing-base.a-spacing-top-base > div.a-column.a-span2.a-text-right.sc-action-links.a-span-last > div > div > input', '999');
+    this.capture('05.png');
+    getHTML('05.html');
+});
+
+casper.waitForSelector('#a-autoid-1-announce', function() {
+    console.log('accepting');
+    this.click('#a-autoid-1-announce');
+    this.capture('06.png');
+    getHTML('06.html');
+});
+
+console.log('waiting for error box 🕑 👀');
+casper.waitForSelector('#activeCartViewForm > div.sc-list-body > div:nth-child(2) > div.sc-list-item-content > div.sc-quantity-update-message.a-spacing-top-mini > div > div', function() {
+    // the stock amount is now the value of the input element
+    var inStock = document.queryselector('#activeCartViewForm > div.sc-list-body > div:nth-child(2) > div.sc-list-item-content > div.a-row.a-spacing-base.a-spacing-top-base > div.a-column.a-span2.a-text-right.sc-action-links.a-span-last > div > div > input').value;
+    console.log(inStock);
+});
+
+casper.then(function() {
+    console.log('selfie! 📷');
+    this.capture('selfie.png');
 });
 
 casper.run();
